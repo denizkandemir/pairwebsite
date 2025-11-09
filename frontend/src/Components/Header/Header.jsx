69e5a4a434f8a5323f6fd68544a8a7e7e6280703
denizkandemir <denizkandemir137@gmail.com>
2025-11-09 14:26:12 +0100

    navbar and Header content added

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,13 +1,21 @@
-import "/Header.scss";
+import "./Header.scss";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import headerImg from "/pairHeader1.png";
 
 const Header = () => {
 
     return(
         <div className="header-container">
             <Navbar />
-           <h1>SCN PAIR</h1>
+           <div className="header-content-container">
+                 <div className="header-texts-container">
+                    <h1 className="header-title"> The ForeFront Of Modern Research </h1>
+                    <p className="header-text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, nesciunt! Fugit totam, laboriosam obcaecati, cum fugiat, vel quidem delectus consequuntur ipsam alias suscipit optio. Iure consequuntur tempore molestias id deserunt. </p>
+                    <button className="header-button"> Read Our Researches </button>
+                 </div>
+                <img src={headerImg} alt="" className="header-img" />
+           </div>
         </div>
     )
 };
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import "/Navbar.scss";
+import "./Navbar.scss";
 import React from "react";
 import { Link } from "react-router-dom";
+import schoolLogo from "/pairSchoolLogo.png";
+import pairLogo from "/pairLogo.png";
 
 const Navbar = () => {
     return (
         <div className="navbar-container">
             <div className="navbar-content-container">
                 <div className="logo-container">
-                    <img src="" alt="" className="logo-img" />
+                    <img src={schoolLogo} alt="" className="logo-img" />
+                    <img src={pairLogo} alt="" className="logo-img" />
                 </div>
                 <div className="navbar-center-container">
                     <Link className="navbar-link" to="/">Blog</Link>
@@ -15,6 +18,15 @@ const Navbar = () => {
                     <Link className="navbar-link"  to="/contact">Contact</Link>
                 </div>
                 <div className="navbar-right-container">
+                    <a href="" className="svg-link">
+                       Insta
+                    </a>
+                    <a href="" className="svg-link">
+                       Face
+                    </a>
+                    <a href="" className="svg-link">
+                       LinkedIn
+                    </a>
 
                 </div>
             </div>
