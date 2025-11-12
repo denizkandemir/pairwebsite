6d16a61ad32b0bba0d1abcc1273dea811c1de637
denizkandemir <denizkandemir137@gmail.com>
2025-11-13 00:51:12 +0100

    icon cards created

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@ import "./Header.scss";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import headerImg from "/pairHeader1.png";
+import WhoAreWe from "../WhoAreWe/WhoAreWe";
 
 const Header = () => {
 
@@ -18,6 +19,7 @@ const Header = () => {
                  </div>
                 <img src={headerImg} alt="" className="header-img" />
            </div>
+              <WhoAreWe />
         </div>
     )
 };
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import Header from "../Components/Header/Header";
 
+
 const HomePage = () => {
     return (
         <div>
