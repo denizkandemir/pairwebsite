88fc460ed28446cd4221dd49b3e4561bf0122164
denizkandemir <denizkandemir137@gmail.com>
2025-11-16 20:35:12 +0100

    blog img added - styles updated

--- frontend/src/Components/Blog/Blog.jsx
+++ frontend/src/Components/Blog/Blog.jsx
@@ -1,6 +1,7 @@
 import "./Blog.scss";
 import React, { use } from "react";
 import useFadeInOnScroll from "../../hooks/FadeInAnimation/FadeInAnimation";
+import blogImg from "/blog3.png";
 
 const Blog = () => {
 
@@ -9,6 +10,9 @@ const Blog = () => {
     return(
         <div className="blog-container">
             <div className="blog-content-container">
+                 <div className="blog-img-container fade-in-right">
+                       <img src={blogImg} alt="" className="blog-img" />
+                </div>
                 <div className="blog-text-container ">
                     <h2 className="blog-title fade-in">
                         Check Out Our Latest Articles and Research Insights
@@ -17,9 +21,7 @@ const Blog = () => {
                         Stay tuned for in-depth analyses, expert opinions, and the latest trends in the industry.
                     </p>
                 </div>
-                <div className="blog-img-container fade-in-right">
-
-                </div>
+               
             </div>
         </div>
     )
