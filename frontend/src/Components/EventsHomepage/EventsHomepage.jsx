532a6e4bf0cdea9095fd2b3b3a126e915fd1b964
denizkandemir <denizkandemir137@gmail.com>
2025-11-18 20:08:35 +0100

    events image added - styles updated

--- frontend/src/Components/EventsHomepage/EventsHomepage.jsx
+++ frontend/src/Components/EventsHomepage/EventsHomepage.jsx
@@ -2,6 +2,12 @@ import "./EventsHomepage.scss";
 import React, { use } from "react";
 import useFadeInOnScroll from "../../hooks/FadeInAnimation/FadeInAnimation";
 import { Link } from "react-router-dom";
+import eventImg1 from "/eventsImage1.png";
+import eventImg2 from "/eventsImage2.png";
+import eventImg3 from "/eventsImage3.png";
+import eventImg4 from "/eventsImage4.png";
+import eventImg5 from "/eventsImage5.png";
+import eventImg6 from "/eventsImage6.png";
 
 const EventsHomepage = () => {
 
@@ -11,13 +17,14 @@ const EventsHomepage = () => {
         <div className="events-homepage-container">
             <div className="events-homepage-content-container">
                 <div className="events-img-container">
-                    <img src="" alt="" className="event-img" />
-                    <img src="" alt="" className="event-img" />
-                    <img src="" alt="" className="event-img" />
+                    <img src={eventImg1} alt="" className="event-img" />
+                    <img src={eventImg3} alt="" className="event-img" />
+                    <img src={eventImg2} alt="" className="event-img" />
                 </div>
 
                 <div className="events-center-container">
                     <div className="events-text-container">
+                        <p className="events-little-title"> PAIR Proudly Organizes </p>
                         <h2 className="events-title"> Join Our Events and Webinars </h2>
                         <p className="events-text"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, illum! Vitae dolorum ducimus, cupiditate, unde numquam itaque architecto totam nemo ab temporibus officia hic molestiae soluta incidunt culpa maxime! Eum? </p>
                     </div>
@@ -31,9 +38,9 @@ const EventsHomepage = () => {
                 </div>
 
                 <div className="events-img-container">
-                    <img src="" alt="" className="event-img" />
-                    <img src="" alt="" className="event-img" />
-                    <img src="" alt="" className="event-img" />
+                    <img src={eventImg5} alt="" className="event-img" />
+                    <img src={eventImg4} alt="" className="event-img" />
+                    <img src={eventImg6} alt="" className="event-img" />
                 </div>
             </div>
         </div>
--- frontend/src/Components/Subscribe/Subscribe.jsx
+++ frontend/src/Components/Subscribe/Subscribe.jsx
@@ -9,25 +9,25 @@ const Subscribe = () => {
     useFadeInOnScroll();
 
     return (
-        <div class="subscribe-container">
-            <div class="subscribe-content-container">
+        <div className="subscribe-container">
+            <div className="subscribe-content-container">
 
-                <div class="subscribe-img-container fade-in-left">
+                <div className="subscribe-img-container fade-in-left">
                    <img src={subscribeMail} alt="" className="subscribe-img" />
                 </div>
 
-                <div class="subscribe-content">
+                <div className="subscribe-content">
                     <h2 className="subscribe-title">Stay in the Loop</h2>
                     <p className="subscribe-text">Psychology & AI research updates delivered monthly.</p>
 
-                    <form class="subscribe-form">
+                    <form className="subscribe-form">
                         <input type="email" placeholder="Enter your email..." />
                         <button type="submit">Subscribe</button>
                     </form>
                 </div>
 
                 
-                <div class="subscribe-img-container fade-in-right">
+                <div className="subscribe-img-container fade-in-right">
                    <img src={subscribePlane} alt="" className="subscribe-img plane-img" />
                 </div>
 
