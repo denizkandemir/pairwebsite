78131639de4a2bcfce273cdfe175e129104054ef
denizkandemir <denizkandemir137@gmail.com>
2025-11-25 19:24:29 +0100

    scrollToTop error fixed

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,9 +1,9 @@
 // import Footer from "./Components/Footer/Footer"
 import { Outlet } from "react-router-dom"
 import { useLocation } from "react-router-dom"
-import ScrollToTop from "./hooks/scrollToTop";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import ScrollToTop from "./hooks/ScrollToTop";
 
 export function Layout() {
 
