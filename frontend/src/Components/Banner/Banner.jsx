8e158df860533da207e43f67a7bf8bde937408e0
denizkandemir <denizkandemir137@gmail.com>
2025-12-05 01:16:10 +0100

    banner designs changed

--- frontend/src/Components/Banner/Banner.jsx
+++ frontend/src/Components/Banner/Banner.jsx
@@ -1,22 +1,23 @@
 import "./Banner.scss";
 
-const Banner = ({title,backgroundImage,text}) => {
+const Banner = ({ title, backgroundImage, text }) => {
     return (
         <div className="banner-container">
             <div className="banner-content-container">
-               <div className="banner-text-container">
-                   <h3 className="banner-title"> 
-                      {title}
-                   </h3>
-                   <p className="banner-text">
-                     {text}
-                   </p>
-               </div>
+                <div className="banner-text-container">
+                    <h3 className="banner-title">
+                        {title}
+                    </h3>
+                    <p className="banner-text">
+                        {text}
+                    </p>
+                </div>
+                <div className="banner-background-img-container">
+                    <img src={backgroundImage} alt="" className="banner-background-img" />
+                </div>
             </div>
-            <div className="banner-background-img-container">
-                <img src={backgroundImage} alt="" className="banner-background-img" />
-            </div>
-        </div>  
+
+        </div>
     )
 }
 
--- frontend/src/Pages/ContactPage.jsx
+++ frontend/src/Pages/ContactPage.jsx
@@ -1,10 +1,12 @@
 import ContactContent from "/src/Components/ContactContent/ContactContent.jsx";
 import Banner from "../Components/Banner/Banner";
+import bannerBackground from "/bannerBackground2.png";
 
 const ContactPage = () => {
     return (
         <>
-         <Banner title="Contact Us" text="We would love to hear from you! Reach out to us with any questions, feedback, or collaboration ideas." />
+         <Banner title="Contact Us" backgroundImage={bannerBackground} text="We would love to hear from you! Reach out to us with any questions, feedback, or collaboration ideas."  />
+         <div className="banner-transition"></div>
          <ContactContent/>
         </>
     )
